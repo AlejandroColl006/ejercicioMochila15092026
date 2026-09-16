03cf2444d8522448ff24344e22159f908f8a3853
Your Name <acolllen@myuax.com>
2026-09-16 16:10:13 +0200

    Creamos la clase Mochila, estableciendo los atributos privados (pesoActual y capacidadMaxima), los 3 constructores, los métodos y los getters para luego ejecutar en la clase Pantalla.java que es el Main.

--- ClaseMochila/Mochila.java
+++ ClaseMochila/Mochila.java
@@ -2,59 +2,69 @@ package ClaseMochila;
 
 public class Mochila {
 
-        private int capacidadMaxima ;
-        private int pesoActual;
+    private int capacidadMaxima;
+    private int pesoActual;
 
-        public Mochila() {
+    public Mochila() {
+        pesoActual = 0;
+        capacidadMaxima = 5000;
+    }
+
+    public Mochila(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        pesoActual = capacidadMaxima / 2;
+    }
+
+    public Mochila(int capacidadMaxima, int pesoActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        if (pesoActual > capacidadMaxima) {
+            this.pesoActual = capacidadMaxima;
+        } else if (pesoActual < 0) {
+            this.pesoActual = 0;
+        } else {
+            this.pesoActual = pesoActual;
+        }
+    }
+
+    public void llenarMochila() {
+        pesoActual = capacidadMaxima;
+    }
+
+    public void sacarObjeto(int peso) {
+        if (peso >= pesoActual) {
             pesoActual = 0;
-            capacidadMaxima = 5000;
+        } else {
+            pesoActual -= peso;
         }
+    }
 
-        public Mochila(int capacidadMaxima) {
-            this.capacidadMaxima = capacidadMaxima;
-            pesoActual = capacidadMaxima / 2;
-        }
-        public Mochila(int capacidadMaxima, int pesoActual) {
-            this.capacidadMaxima = capacidadMaxima;
-            if (pesoActual > capacidadMaxima) {
-                this.pesoActual = capacidadMaxima;
-            } else if (pesoActual < 0) {
-                this.pesoActual = 0;
-            } else {
-                this.pesoActual = pesoActual;
-            }
-        }
+    public void vaciarMochila() {
+        pesoActual = 0;
+    }
 
-        public void llenarMochila(){
+    public void meterObjeto(int peso) {
+        if (pesoActual + peso > capacidadMaxima) {
             pesoActual = capacidadMaxima;
+        } else {
+            pesoActual += peso;
         }
-        public void sacarObjeto(int peso){
-            if (peso >= pesoActual) {
-                pesoActual = 0;
-            } else {
-                pesoActual -= peso;
-            }
-        }
+    }
 
-        public void vaciarMochila(){
-            pesoActual = 0;
-        }
-        public void meterObjeto(int peso){
-            if (pesoActual + peso > capacidadMaxima) {
-                pesoActual = capacidadMaxima;
-            } else {
-                pesoActual += peso;
-            }
-        }
+    public int getPesoActual() {
+        return pesoActual;
+    }
 
-        public int getPesoActual() {
-            return pesoActual;
-        }
-        public int getCapacidadMaxima() {
-            return capacidadMaxima;
-        }
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
 
-    public static void main() {
+    public String mostrarEstado() {
+        return "La mochila pesa " + pesoActual +
+                " g de un máximo de " + capacidadMaxima + " g";
+    }
 
+    @Override
+    public String toString() {
+            return "Mochila[pesoActual=" + pesoActual + "g, capacidadMaxima=" + capacidadMaxima + "g]";
     }
 }
